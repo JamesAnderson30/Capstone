b6f848e5547dfa69a35c38ef096d44b5b1407487
James Anderson <jamesanderson_18@hotmail.com>
2025-03-01 15:46:43 -0500

    Added a reply tag element

--- react-vite/src/components/Posts/Components/PostReplyTag.jsx
+++ react-vite/src/components/Posts/Components/PostReplyTag.jsx
@@ -1,6 +1,13 @@
 
 function PostReplyTag({parentPost}){
-    console.log("parentPost: ", parentPost)
+    let postName = ""
+    if(parentPost.Topic_Profile) postName = parentPost.Topic_Profile.name
+    else postName = parentPost.username
+    return (
+        <div className={"PostReplyTag"}>
+            Reply to {postName} <a href={`#post${parentPost.id}`}>View</a>
+        </div>
+    )
 }
 
 export default PostReplyTag;
